1c54f5f049e0ae910abcd961c5d4105dc49c3703
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 15:55:14 -0300

    feat: validate E2E test

--- src/products.test.ts
+++ src/products.test.ts
@@ -4,6 +4,9 @@ import { app } from "./app";
 describe("products", () => {
   it("should list products", async () => {
     const response = await request(app).get("/products");
-    console.log(response.body);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toHaveLength(3);
+    expect(response.body.length).toBeGreaterThan(0);
   });
 });
