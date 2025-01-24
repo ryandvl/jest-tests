8dd6bddfd99a068f29e4deeb105be94d72b17304
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 15:32:26 -0300

    feat: beforeAll function

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,10 +1,16 @@
 import { sum } from "./server";
 
 describe("sum", () => {
+  let sumResult: number;
+
+  beforeAll(() => {
+    console.log("Executing before all tests");
+    sumResult = 10;
+  });
+
   it("should do sum of 3 + 7 must be 10", () => {
     const result = sum(3, 7);
 
-    // expect(result).toBe(9);
     expect(result).toBe(10);
   });
 
@@ -14,11 +20,3 @@ describe("sum", () => {
     expect(result).toBe(4);
   });
 });
-
-// describe("sum 2", () => {
-//   test("sum of 2 + 2 must be 4", () => {
-//     const result = sum(2, 2);
-
-//     expect(result).toBe(4);
-//   });
-// });
