198028f773890c39fb1fcad648f543e7b9e7ca42
ryandvl <contatoryandvl55@gmail.com>
2025-01-24 15:23:08 -0300

    feat: grouping tests with describe function

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,8 +1,24 @@
 import { sum } from "./server";
 
-test("sum of 3 + 7 must be 10", () => {
-  const result = sum(3, 7);
+describe("sum", () => {
+  test("sum of 3 + 7 must be 10", () => {
+    const result = sum(3, 7);
 
-  // expect(result).toBe(9);
-  expect(result).toBe(10);
+    // expect(result).toBe(9);
+    expect(result).toBe(10);
+  });
+
+  test("sum of 2 + 2 must be 4", () => {
+    const result = sum(2, 2);
+
+    expect(result).toBe(4);
+  });
 });
+
+// describe("sum 2", () => {
+//   test("sum of 2 + 2 must be 4", () => {
+//     const result = sum(2, 2);
+
+//     expect(result).toBe(4);
+//   });
+// });
